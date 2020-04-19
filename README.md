b436d6557e8db9db71a01bb3c4947bab891b5dfe
Ocky Aditia Saputra <ocky.aditia@tokopedia.com>
2020-04-19 12:51:24 +0700

    Update README.md

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 ###
 ###
 ###
+A quick brown fox jumps over the lazy dog
+lower then
---
+++ entrypoint.sh
@@ -0,0 +1,5 @@
+fi
+
+# This is a special comment, please do not touch: we use it for tests.
+# test: lower then
+# A quick brown fox jumps over the lazi dog
